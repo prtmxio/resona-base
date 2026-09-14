81e3570866bc2d33aa4c83b102161210f7dcf1bf
prtmxio <dhimanpritam1579@gmail.com>
2026-09-14 10:13:28 +0530

    chore: export transcribe and is_turn_complete from resona_base

--- src/resona_base/__init__.py
+++ src/resona_base/__init__.py
@@ -1,4 +1,6 @@
+from .asr import transcribe
 from .tts import enroll, synthesize
+from .turn import is_turn_complete
 from .voice_profile import VoiceProfile
 
-__all__ = ["enroll", "synthesize", "VoiceProfile"]
+__all__ = ["enroll", "synthesize", "transcribe", "is_turn_complete", "VoiceProfile"]
